701ed5b7716140f2e82b23a142ef5483755f6452
harvash <harvash2004@yahoo.com>
2020-06-12 19:52:48 -0400

    First commit

---
+++ vars.tf
@@ -0,0 +1,22 @@
+# var.tf
+# Variable definitions
+
+# general
+
+
+# Client & Subscription Data
+variable "azure_sub_id" {
+  default = ""
+}
+
+variable "azure_client_id" {
+  default = ""
+}
+
+variable "azure_client_secret" {
+  default = ""
+}
+
+variable "azure_tenant_id" {
+  default = ""
+}
