f1593b5caf4fa62e347188e8f7da9a30085fa456
harvash <harvash2004@yahoo.com>
2020-06-21 14:33:50 -0400

    update for LoadBalancer service

---
+++ terraform/providers.tf
@@ -0,0 +1,41 @@
+# providers.tf
+# Resource definitions for providers
+
+# Configure the Microsoft Azure Resource Manager provider
+provider "azurerm" {
+  version = ">= 2.0.0"
+  features {}
+  subscription_id = var.azure_sub_id
+  tenant_id       = var.azure_tenant_id
+
+  # Allows to access the subscription resource group through a service principal
+  #
+  client_id     = var.azure_client_id
+  client_secret = var.azure_client_secret
+
+  skip_provider_registration  = true
+}
+
+# Configure the Microsoft Azure Active Directory provider
+provider "azuread" {
+  version = "= 0.4.0"
+
+  subscription_id = var.azure_sub_id
+  tenant_id       = var.azure_tenant_id
+
+  # Allows to access the subscription resource group through a service principal
+  #
+  client_id     = var.azure_client_id
+  client_secret = var.azure_client_secret
+}
+
+# provider "random" {
+#   version = ">= 2.1"
+# }
+
+# provider "null" {
+#   version = ">= 2.1"
+# }
+
+# Retrieve details for the subscription
+data "azurerm_subscription" "subscription" {}
